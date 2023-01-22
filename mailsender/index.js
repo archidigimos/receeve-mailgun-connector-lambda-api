f01e76a8da7cad055962468fd4a0dec5b735df74
Archisman Sarkar <archidehex@gmail.com>
2023-01-22 13:58:15 +0530

    redacted redundant credentials

--- mailsender/index.js
+++ mailsender/index.js
@@ -1,5 +1,5 @@
-const API_KEY = '95f82492ce03039eac0d6dbc97227773-f7d687c0-50af170f';
-const DOMAIN = 'sandboxbd19c99b15754db8aac2ac1d698c92c0.mailgun.org';
+const API_KEY = '';
+const DOMAIN = '';
 
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
@@ -8,8 +8,8 @@ const mailgun = new Mailgun(formData);
 const client = mailgun.client({ username: 'api', key: API_KEY });
 
 const messageData = {
-    from: 'archismansarkar@sandboxbd19c99b15754db8aac2ac1d698c92c0.mailgun.org',
-    to: 'archidehex@gmail.com',
+    from: '',
+    to: '',
     subject: 'Hello',
     text: 'Testing some Mailgun awesomeness!'
 };
