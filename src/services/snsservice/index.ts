7260f7f3e761cf664538aa9b3cbec23352da6c80
Archisman Sarkar <archidehex@gmail.com>
2023-01-22 22:46:21 +0530

    minor tweaks

--- src/services/snsservice/index.ts
+++ src/services/snsservice/index.ts
@@ -12,7 +12,7 @@ const sendMessage = async (data: LambdaMailgun, logger: any) => {
         const result = await sns.publish(params).promise();
         logger.info(`Message sent to SNS topic: ${result.MessageId}`);
         return ({
-            snsstatus: `Message Successfully sent to SNS topic: ${result.MessageId}`,
+            snsstatus: `Message Successfully sent to SNS topicId: ${result.MessageId}`,
             params
         });
     } catch (err) {
